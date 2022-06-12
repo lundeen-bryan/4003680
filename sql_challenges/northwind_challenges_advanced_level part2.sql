d0a4d6c45aa2408c20d52bd3f01de83978664d3e
lundeen-bryan <lundeen-bryan@gmail.com>
2022-06-11 20:53:14 -0700

    setup for advanced northwind pt 2

---
+++ sql_challenges/northwind_challenges_advanced_level part2.sql
@@ -0,0 +1,23 @@
+--/*
+SELECT
+ [alias] = table_alias.[column]
+FROM
+ [schema].[table] AS table_alias
+WHERE 1=1
+;
+--*/
+
+
+
+/*
+  Filename: ...: northwind_challenges_advanced_level part2.sql
+  Date ........: 11-Jun-2022
+  Time ........: 17:29
+  Desc ........: Part 2 of the advanced
+  Notes........:
+    1.
+*/
+
+
+
+GO
